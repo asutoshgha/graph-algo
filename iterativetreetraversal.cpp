ff04dce5c29cc67962be5551ba800c983e9e4d73
ASUTOSH GHANTO <44160439+asutoshgha@users.noreply.github.com>
2021-04-06 23:25:02 +0530

    Update iterativetreetraversal.cpp

--- iterativetreetraversal.cpp
+++ iterativetreetraversal.cpp
@@ -7,13 +7,13 @@ struct node{
   int data;
   node* right;
 };
-//if you need a way to do it without any recursion
+//if you need a way to do it without any recursion but use some memory in unordered map
 stack<node*> s;
 unordered_map<node*,int> cnt;
 void inorderiterative(node* root){
   s.push(root);
   while(!s.empty()){
-      node* curr=s.pop()
+      node* curr=s.top()
       if(curr == null) {s.pop(); continue;}
       if(cnt[curr] == 0)s.push(curr->left);
       else if(cnt[curr] == 1)cout<<curr->data<<" ";
@@ -21,6 +21,16 @@ void inorderiterative(node* root){
       else s.pop();
    }
 }
+stack<pair<node*,int>> s;
+void inordertraversalwithoutunorderedmap(node* root){
+  s.push({root,0});
+  while(!s.empty()){
+    auto curr=s.top();
+    
+  }
+  
+}
+
 int main(){
 
 }
