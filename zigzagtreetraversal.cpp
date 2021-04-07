3b0011cef6e330168d7982be8c26bcf3c3ba0596
ASUTOSH GHANTO <44160439+asutoshgha@users.noreply.github.com>
2021-04-07 09:21:47 +0530

    Create zigzagtreetraversal.cpp

---
+++ zigzagtreetraversal.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+struct Node{
+  int data;
+  struct Node *left,*right;
+}
+
+struct Node* newNode(int data)
+{
+    struct Node* node = new struct Node;
+    node->data = data;
+    node->left = node->right = NULL;
+    return (node);
+}
+
+void zigzagtreetraversal(Node* root){
+  stack<Node*> currlevel;
+  stack<Node*> nextlevel;
+  currlevel.push(root);
+  bool lefttoright=true;
+  while(!currlevel.empty()){
+    Node* temp=currlevel.top();
+    currlevel.pop();
+    if(temp){
+      cout<<temp->data<<" ";
+      if(lefttoright){
+        if(temp->left){
+            nextlevel.push(temp->left);
+        }
+        if(temp->right){
+          nextlevel.push(temp->right);
+        }
+      }
+      else{
+       if(temp->right){
+            nextlevel.push(temp->right);
+        }
+        if(temp->left){
+            nextlevel.push(temp->left);
+        }
+      }
+    
+    }
+  }
+}
+
+int main(){
+  
+  struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(7);
+    root->left->right = newNode(6);
+    root->right->left = newNode(5);
+    root->right->right = newNode(4);
+    cout << "ZigZag Order traversal of binary tree is \n";
+ 
+    zizagtraversal(root);
+ 
+    return 0;
+
+}
