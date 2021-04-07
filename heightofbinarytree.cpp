835d91e960bb1f53a81cf295cbc017bf1bb553fc
ASUTOSH GHANTO <44160439+asutoshgha@users.noreply.github.com>
2021-04-07 12:13:26 +0530

    Create heightofbinarytree.cpp

---
+++ heightofbinarytree.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+struct Node {
+  int data;
+  struct Node *left,*right;
+};
+
+int heightoftree(Node* root){
+  if(root == null)
+    return 0;
+  int left=heightoftree(root->left);
+  int right=heightoftree(root->right);
+  if(left>right){
+    return left+1;
+  }
+  else{
+  return right+1;
+  }
+}
+
+Node* newNode(int data)
+{
+    Node* Node = new Node();
+    Node->data = data;
+    Node->left = NULL;
+    Node->right = NULL;
+     
+    return(Node);
+}
+int main(){
+  Node *root = newNode(1);
+ 
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+     
+    cout << "Height of tree is " << maxDepth(root);
+    return 0;
+}
